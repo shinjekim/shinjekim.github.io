b167b8d49140dcfc59c51ac93002c495d16e7b2f
ShinJe Kim <shinjehere@gmail.com>
2024-11-21 11:32:24 +1100

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -4,3 +4,13 @@ const homeHeight = home.offsetHeight;
 document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+// Arrow: Makes the arrow button transparent when scrolling down
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+});
