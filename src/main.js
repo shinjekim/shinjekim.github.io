5accca1be5470beb5ba426d08df8ecd3024d3318
ShinJe Kim <shinjehere@gmail.com>
2024-11-21 11:23:25 +1100

    JS-Home

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// Home: Makes the home section transparent when scrolled down
+const home = document.querySelector(".home__container");
+const homeHeight = home.offsetHeight;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
