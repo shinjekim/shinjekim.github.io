7afb01ee5026b360efdb540f7cb8ea9a8f138dfe
ShinJe Kim <shinjehere@gmail.com>
2024-11-21 12:01:12 +1100

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -14,3 +14,15 @@ document.addEventListener("scroll", () => {
     arrowUp.style.opacity = 0;
   }
 });
+
+// Navbar: Handles Navbar toggle button click event
+const navbarMenu = document.querySelector(".header__menu");
+const navbarToggle = document.querySelector(".header__toggle");
+navbarToggle.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
+});
+
+// Navbar: Closes automatically the Navbar menu when a menu item is clicked
+navbarMenu.addEventListener("click", () => {
+  navbarMenu.classList.remove("open");
+});
